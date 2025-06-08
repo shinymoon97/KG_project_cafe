1d1f7b9fa3b0b02db31887200b25abf09e574b78
shinymoon97 <shinymoon97@naver.com>
2025-06-08 14:35:32 +0900

    Update HomeController.java

--- CafeOrderApp/src/main/java/com/example/demo/controller/HomeController.java
+++ CafeOrderApp/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to the Cafe Order App!";
+        return "index";  // templates/index.html
     }
 }
