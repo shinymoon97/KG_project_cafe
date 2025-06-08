a5700260023d8d4af5fb41dd097fdc9c4f103c7c
shinymoon97 <shinymoon97@naver.com>
2025-06-08 17:34:33 +0900

    Update CafeOrderAppApplication.java

--- CafeOrderApp/src/main/java/com/example/demo/CafeOrderAppApplication.java
+++ CafeOrderApp/src/main/java/com/example/demo/CafeOrderAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CafeOrderAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CafeOrderAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CafeOrderAppApplication.class, args);
+    }
 }
