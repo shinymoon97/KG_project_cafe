3a1a4dc2e7fe502fe0e49f7f15e0f4ba1f3014bf
shinymoon97 <shinymoon97@naver.com>
2025-06-08 17:58:57 +0900

    Update SaleController.java

--- CafeOrderApp/src/main/java/com/example/demo/sale/controller/SaleController.java
+++ CafeOrderApp/src/main/java/com/example/demo/sale/controller/SaleController.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/menu")
+@RequestMapping("/api/menu")
 public class SaleController {
 
     private final SaleService saleService;
