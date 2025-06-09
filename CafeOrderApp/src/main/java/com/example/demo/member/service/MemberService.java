78ef74f82dc02dd4bbf49f929271d949657853fd
shinymoon97 <shinymoon97@naver.com>
2025-06-09 10:16:07 +0900

    Update MemberService.java

--- CafeOrderApp/src/main/java/com/example/demo/member/service/MemberService.java
+++ CafeOrderApp/src/main/java/com/example/demo/member/service/MemberService.java
@@ -1,31 +1,21 @@
 package com.example.demo.member.service;
 
+import com.example.demo.member.dao.MemberDAO;
 import com.example.demo.member.dto.MemberDTO;
-import com.example.demo.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MemberService {
 
-    private final MemberRepository memberRepository;
-
     @Autowired
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    private MemberDAO memberDAO;
+
+    public void registerMember(MemberDTO member) {
+        memberDAO.insertMember(member);
     }
 
-    public MemberDTO saveMember(MemberDTO member) {
-        return memberRepository.save(member);
-    }
-
-    public List<MemberDTO> getAllMembers() {
-        return memberRepository.findAll();
-    }
-
-    public MemberDTO findByEmail(String email) {
-        return memberRepository.findByEmail(email);
+    public MemberDTO login(String email, String password) {
+        return memberDAO.findByEmailAndPassword(email, password);
     }
 }
