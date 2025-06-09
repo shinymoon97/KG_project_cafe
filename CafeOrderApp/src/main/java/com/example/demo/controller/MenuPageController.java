ea63649e8178e2dbffd411db0fd3c6decc7a6ecb
shinymoon97 <shinymoon97@naver.com>
2025-06-09 10:20:44 +0900

    Update MenuPageController.java

--- CafeOrderApp/src/main/java/com/example/demo/controller/MenuPageController.java
+++ CafeOrderApp/src/main/java/com/example/demo/controller/MenuPageController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MenuPageController {
 
-    @GetMapping("/menu-page")
-    public String menuPage() {
-        return "menu"; // templates/menu.html
+    @GetMapping("/menuPage")
+    public String showMenuPage() {
+        return "menu"; // → templates/menu.html 로 이동
     }
 }
