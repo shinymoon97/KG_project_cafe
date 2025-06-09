086c0d6e8f1659b3d5ee6d94bb9dce5867f750ce
shinymoon97 <shinymoon97@naver.com>
2025-06-09 10:21:25 +0900

    Update MemberDTO.java

--- CafeOrderApp/src/main/java/com/example/demo/member/dto/MemberDTO.java
+++ CafeOrderApp/src/main/java/com/example/demo/member/dto/MemberDTO.java
@@ -1,55 +1,17 @@
 package com.example.demo.member.dto;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "members")
 public class MemberDTO {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String name;
     private String email;
     private String password;
 
-    // 기본 생성자
-    public MemberDTO() {}
+    // Getter / Setter
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    // 생성자
-    public MemberDTO(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    // Getter & Setter
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
